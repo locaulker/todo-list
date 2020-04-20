415300896ba022f9e576d1cc565e618d088ef35a
Lawrence <locaulker@gmail.com>
2020-04-19 22:45:19 -0400

    completed todolist component and clear list

--- src/App.js
+++ src/App.js
@@ -25,18 +25,17 @@ class App extends Component {
 			title: this.state.item,
 		}
 		const updatedItems = [...this.state.items, newItem]
-		this.setState(
-			{
-				items: updatedItems,
-				item: "",
-				id: uuidv4(),
-				editItem: false,
-			},
-			() => console.log(this.state)
-		)
+		this.setState({
+			items: updatedItems,
+			item: "",
+			id: uuidv4(),
+			editItem: false,
+		})
 	}
 	clearList = () => {
-		console.log("Clear list")
+		this.setState({
+			items: [],
+		})
 	}
 	handleDelete = id => {
 		console.log(`handle delete ${id}`)
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,6 +2,19 @@ import React, {Component} from "react"
 
 export default class TodoItem extends Component {
 	render() {
-		return <div>Hello from Todo item</div>
+		const {title, handleDelete, handleEdit} = this.props
+		return (
+			<li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+				<h6>{title}</h6>
+				<div className="todo-icon">
+					<span className="mx-2 text-success" onClick={handleEdit}>
+						<i className="fad fa-edit"></i>
+					</span>
+					<span className="mx-2 text-danger" onClick={handleDelete}>
+						<i className="fad fa-trash-alt"></i>
+					</span>
+				</div>
+			</li>
+		)
 	}
 }
